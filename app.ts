305602623f0cd85eec96a746d44ee790670ae868
Hebbar <prajwalhebbaras@gmail.com>
2024-10-15 18:27:10 +0530

    understand type inference and explicit type assignment.
    Understood how type inference happens and how in certain cases it is not
    the right approach. In cases where type inference won't work, understood
    how to use explicit type assignment to overcome the situation.

---
+++ app.ts
@@ -0,0 +1,11 @@
+let username = "Prajwal";
+
+// username = 36 - will throw an error
+
+let lastname;
+lastname = 33;
+lastname = "Hebbar";
+
+let firstname: string;
+// firstname = 33 - will throw an error
+firstname = "Prajwal";
