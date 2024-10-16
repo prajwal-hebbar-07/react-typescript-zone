134ce0d2ad29195f508c6c69a516b0b9365ef401
Hebbar <prajwalhebbaras@gmail.com>
2024-10-16 12:22:22 +0530

    understand object type
    Understanding different ways of defining object types and what are the
    uses and cons of different type assignments.

--- app.ts
+++ app.ts
@@ -18,3 +18,36 @@ let justBoolean: boolean;
 // union of types
 let userId: string | number = "abc123";
 userId = 123;
+
+// object types
+// let user;
+// user = {
+//   name: "Prajwal",
+//   age: 25,
+//   isAvailable: true,
+//   id: 123, // "abc"
+// };
+
+let user: object;
+user = {
+  name: "Prajwal",
+};
+
+user = {
+  age: 12,
+};
+
+let user2: {
+  name: string;
+  age: number;
+  isAvailable: boolean;
+  id: string | number;
+};
+
+// user2 = {} - Error
+user2 = {
+  name: "Prajwal",
+  age: 25,
+  isAvailable: false,
+  id: 123,
+};
