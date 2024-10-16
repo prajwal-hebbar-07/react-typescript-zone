b226e8ac3082d0095ba71fa7b841904cb9ab8827
Hebbar <prajwalhebbaras@gmail.com>
2024-10-16 12:26:39 +0530

    understand array types
    Understanding how array types work and how a related type should also be
    given to an array type.

--- app.ts
+++ app.ts
@@ -1,53 +1,9 @@
-let username = "Prajwal";
+//array types
 
-// username = 36 - will throw an error
+let hobbies: Array<string>;
 
-let lastname;
-lastname = 33;
-lastname = "Hebbar";
+hobbies = ["Sports", "Music"];
 
-let firstname: string;
-// firstname = 33 - will throw an error
-firstname = "Prajwal";
+let hobbies2: string[]; // number[], boolean[], {name: string}[]
 
-// Basic Primitive Types
-let justString: string;
-let justNumber: number;
-let justBoolean: boolean;
-
-// union of types
-let userId: string | number = "abc123";
-userId = 123;
-
-// object types
-// let user;
-// user = {
-//   name: "Prajwal",
-//   age: 25,
-//   isAvailable: true,
-//   id: 123, // "abc"
-// };
-
-let user: object;
-user = {
-  name: "Prajwal",
-};
-
-user = {
-  age: 12,
-};
-
-let user2: {
-  name: string;
-  age: number;
-  isAvailable: boolean;
-  id: string | number;
-};
-
-// user2 = {} - Error
-user2 = {
-  name: "Prajwal",
-  age: 25,
-  isAvailable: false,
-  id: 123,
-};
+hobbies2 = ["sports"];
