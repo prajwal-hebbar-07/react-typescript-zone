fe981d950e7db4adacea3b46c3695280518279b7
Hebbar <prajwalhebbaras@gmail.com>
2024-10-16 18:25:28 +0530

    add type guards for variables that has union types
    Understanding how and why we need to add type guards to the variables or
    functions of the things that have union types.

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
 // literal types
 
-let role: "admin" | "user";
+type Role = "admin" | "user" | "editor";
+
+let role: Role;
 
 role = "admin";
 role = "user";
 
 // role = "editor" - error.
+
+// adding type guards
+
+function performSomething(something: string | number, role: Role) {
+  if (role === "admin" && typeof something === "string") {
+    //...
+  }
+}
