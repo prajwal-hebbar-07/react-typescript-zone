c3392de5362d39a8ece3c72490c91dcca935c482
Hebbar <prajwalhebbaras@gmail.com>
2024-10-16 17:35:29 +0530

    understand types for function arguments and function return types
    Understanding how types work in function arguments and how undefined &
    void can be used as function return types.

--- app.ts
+++ app.ts
@@ -10,3 +10,14 @@ hobbies2 = ["sports"];
 
 // constant types
 const username = "Prajwal";
+
+// function argument & return types
+function add(a: number, b: number): undefined {
+  const result = a + b;
+  console.log(result);
+}
+
+function subtract(a: number, b: number): void {
+  const result = a - b;
+  console.log(result);
+}
