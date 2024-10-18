b282d6f6ebf7f76e7654a1b6d13a523da317b162
Hebbar <prajwalhebbaras@gmail.com>
2024-10-18 17:32:40 +0530

    add state to add goals
    Added state to handle the goals array so that it can be added and
    displayed dynamically.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+interface CourseGoals {
+  title: string;
+  description: string;
+  id: number;
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoals[]>([]);
+
+  function addGoalHandler() {
+    setGoals((prevGoal) => {
+      const goal: CourseGoals = {
+        id: Math.random(),
+        title: "React + TS",
+        description:
+          "Try react with typescript to make the applications more robust!",
+      };
+
+      return [...prevGoal, goal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalImg, alt: "React + TS" }}>
         <h1>React + TS</h1>
       </Header>
-      <CourseGoal title="React + TS">
-        <p>Try react with typescript to make the applications more robust!</p>
-      </CourseGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
