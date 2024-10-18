bdc5243481defc64ebeadae20bc7010375bff645
Hebbar <prajwalhebbaras@gmail.com>
2024-10-18 16:25:56 +0530

    add types to the props passed to the component
    Added types to the props that are being passed down to the component
    from the parent component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal
+        title="React + TS"
+        description="Try react with typescript to make the applications more robust!"
+      />
     </main>
   );
 }
