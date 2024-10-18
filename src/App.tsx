5be6b642eb6ae7377f86cbf4ffaa6f6cbc0911c0
Hebbar <prajwalhebbaras@gmail.com>
2024-10-18 16:35:20 +0530

    add type for children prop
    Added type for the default children prop in that will be sent to all the
    react components by default.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="React + TS"
-        description="Try react with typescript to make the applications more robust!"
-      />
+      <CourseGoal title="React + TS">
+        <p>Try react with typescript to make the applications more robust!</p>
+      </CourseGoal>
     </main>
   );
 }
