866b8e98960700e73dcb8d152e2c338061e47817
Hebbar <prajwalhebbaras@gmail.com>
2024-10-18 17:20:59 +0530

    add header component
    Added a header component with types defined for all the props that are
    received by the header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+import goalImg from "./assets/goals.jpg";
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalImg, alt: "React + TS" }}>
+        <h1>React + TS</h1>
+      </Header>
       <CourseGoal title="React + TS">
         <p>Try react with typescript to make the applications more robust!</p>
       </CourseGoal>
