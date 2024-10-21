74ef01ccf762c108f3ab57430ef220975b46b202
Hebbar <prajwalhebbaras@gmail.com>
2024-10-21 08:35:21 +0530

    add logic for info and warning box
    Added logic to display info and warning boxes based on certain
    condition.

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -2,6 +2,7 @@ import { type FC } from "react";
 
 import { type CourseGoal as CGoal } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 interface CourseGoalListProps {
   goals: CGoal[];
@@ -9,16 +10,35 @@ interface CourseGoalListProps {
 }
 
 const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDeleteGoal }) => {
+  let warningBox;
+
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You don't have any goals. Add some goals!</InfoBox>
+    );
+  }
+
+  if (goals.length >= 3) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You are adding too many goals. Rethink the plan.
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
